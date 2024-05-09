571b94bbf367a3a9802a38dc487422c046d8de61
Matthias Ruiz <matthias.ruiz@blue-chestnut.com>
2024-05-09 17:42:38 +0200

    support sizes and different cap of keywords

--- src/model/table_expr.rs
+++ src/model/table_expr.rs
@@ -1,3 +1,6 @@
+use std::fmt::Debug;
+use std::str::FromStr;
+
 pub struct TableDef {
     pub table_ref: String,
     pub columns: Vec<Box<ColumnDef>>,
@@ -6,7 +9,51 @@ pub struct TableDef {
 
 pub struct ColumnDef {
     pub name: String,
-    pub data_type: String,
+    pub data_type: DataType,
+    pub not_null: bool,
+    pub primary_key: bool,
+}
+
+impl Default for ColumnDef {
+    fn default() -> Self {
+        Self {
+            name: String::new(),
+            data_type: DataType::default(),
+            not_null: false,
+            primary_key: false,
+        }
+    }
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct DataType {
+    pub name: String,
+    pub size : Option<[Option<u32>;2]>
+}
+
+impl Default for DataType {
+    fn default() -> Self {
+        Self {
+            name: String::new(),
+            size: None
+        }
+    }
+}
+
+impl DataType {
+    pub fn f_name(name: &str) -> Self {
+        Self {
+            name: String::from(name),
+            size: None
+        }
+    }
+
+    pub fn f_name_1_size(name: &str, size1: u32) -> Self {
+        Self {
+            name: String::from(name),
+            size: Some([Some(size1), None])
+        }
+    }
 }
 
 pub struct TableLevelRule {
--- src/parser/create_table_tests.rs
+++ src/parser/create_table_tests.rs
@@ -7,27 +7,39 @@ lalrpop_mod!(pub table, "/parser/create_table.rs");
 pub mod create_table_tests {
     use super::*;
     use crate::model::table_expr::ColumnDef;
+    use crate::model::table_expr::DataType;
 
     #[rstest]
     #[case("CREATE TABLE IF NOT EXISTS Inventory {Id INT,Title VARCHAR,  };", "Inventory",
     vec![
-        ColumnDef {name: String::from("Id"), data_type: String::from("INT")},
-        ColumnDef {name: String::from("Title"), data_type: String::from("VARCHAR")},
+        ColumnDef {name: String::from("Id"), data_type: DataType::f_name("INT"), ..Default::default()},
+        ColumnDef {name: String::from("Title"), data_type: DataType::f_name("VARCHAR"), ..Default::default()},
     ])]
     #[case("CREATE TABLE IF NOT EXISTS\n Test {Id FLOAT};", "Test",
     vec![
-    ColumnDef {name: String::from("Id"), data_type: String::from("FLOAT")},
+    ColumnDef {name: String::from("Id"), data_type: DataType::f_name("FLOAT"), ..Default::default()},
     ])]
     #[case(" CREATE TABLE IF NOT EXISTS\n Test \n{\nId FLOAT\n,}\n;\n", "Test",
     vec![
-    ColumnDef {name: String::from("Id"), data_type: String::from("FLOAT")},
+    ColumnDef {name: String::from("Id"), data_type: DataType::f_name("FLOAT"), ..Default::default()},
+    ])]
+    #[case(" create table if not exists\n Test \n{\nId FLOAT\n,}\n;\n", "Test",
+    vec![
+    ColumnDef {name: String::from("Id"), data_type: DataType::f_name("FLOAT"), ..Default::default()},
     ])]
     #[case(" CREATE TABLE IF NOT EXISTS\n Test33 \n{\nId FLOAT,\nPrice FLOAT,\nNotes TEXT\n}\n;\n",
     "Test33",
     vec![
-    ColumnDef {name: String::from("Id"), data_type: String::from("FLOAT")},
-    ColumnDef {name: String::from("Price"), data_type: String::from("FLOAT")},
-    ColumnDef {name: String::from("Notes"), data_type: String::from("TEXT")},
+    ColumnDef {name: String::from("Id"), data_type: DataType::f_name("FLOAT"), ..Default::default()},
+    ColumnDef {name: String::from("Price"), data_type: DataType::f_name("FLOAT"), ..Default::default()},
+    ColumnDef {name: String::from("Notes"), data_type: DataType::f_name("TEXT"), ..Default::default()},
+    ])]
+    #[case(" create table\n Test33 \n{\nId FLOAT PRIMARY KEY,\nPrice FLOAT,\nNotes TEXT not null\n}\n;\n",
+    "Test33",
+    vec![
+    ColumnDef {name: String::from("Id"), data_type: DataType::f_name("FLOAT"), primary_key: true, not_null: true},
+    ColumnDef {name: String::from("Price"), data_type: DataType::f_name("FLOAT"), ..Default::default()},
+    ColumnDef {name: String::from("Notes"), data_type: DataType::f_name("TEXT"), not_null: true,..Default::default()},
     ])]
     fn test_create_table_success(
         #[case] input_value: &str,
@@ -62,14 +74,23 @@ pub mod create_table_tests {
     }
 
     #[rstest]
-    #[case("Id INT", "Id", "INT")]
-    #[case("Id  INT ", "Id", "INT")]
-    #[case(" Id INT", "Id", "INT")]
-    #[case(" Id33 INT", "Id33", "INT")]
+    #[case("Id INT", "Id", DataType::f_name("INT"), false, false)]
+    #[case("Id  INT ", "Id", DataType::f_name("INT"), false, false)]
+    #[case(" Id INT", "Id", DataType::f_name("INT"), false, false)]
+    #[case(" Id33 INT", "Id33", DataType::f_name("INT"), false, false)]
+    #[case(" Id33 INT PRIMARY KEY", "Id33", DataType::f_name("INT"), true, true)]
+    #[case(" Id33 INT primaRY KeY", "Id33", DataType::f_name("INT"), true, true)]
+    #[case(" Id33 TEXT NOT NULL", "Id33", DataType::f_name("TEXT"), true, false)]
+    #[case(" Id33 DOUBLE not null", "Id33", DataType::f_name("DOUBLE"), true, false)]
+    #[case(" Id33 DOUBLE not NuLL", "Id33", DataType::f_name("DOUBLE"), true, false)]
+    #[case(" Id33 DOUBLE(30) not NuLL", "Id33", DataType::f_name_1_size("DOUBLE", 30), true, false)]
+    #[case(" Id33 DOUBLE(30,5) not NuLL", "Id33", DataType {name: String::from("DOUBLE"), size: Some([Some(30), Some(5)])}, true, false)]
     fn test_column_def_success(
         #[case] input_value: &str,
         #[case] name: &str,
-        #[case] date_type: &str,
+        #[case] date_type: DataType,
+        #[case] not_null: bool,
+        #[case] primary_key: bool,
     ) {
         let parsed_result = table::ColumnDefExprParser::new().parse(input_value);
         let parsed_result_ref = parsed_result.as_ref();
@@ -78,8 +99,11 @@ pub mod create_table_tests {
 
         let column_def = parsed_result_ref.unwrap().as_ref();
 
-        assert_eq!(column_def.data_type, date_type);
+        assert_eq!(column_def.data_type.name, date_type.name);
+        assert_eq!(column_def.data_type.size, date_type.size);
         assert_eq!(column_def.name, name);
+        assert_eq!(column_def.not_null, not_null);
+        assert_eq!(column_def.primary_key, primary_key);
     }
 
     #[rstest]
@@ -87,6 +111,9 @@ pub mod create_table_tests {
     #[case("'22''")]
     #[case("3Id INT")]
     #[case("Id, INT")]
+    #[case("Id INT PRIMARY")]
+    #[case("Id INT NOT")]
+    #[case("Id TEXT NULL")]
     fn test_column_def_failure(#[case] input_value: &str) {
         assert!(table::ColumnDefExprParser::new()
             .parse(input_value)
